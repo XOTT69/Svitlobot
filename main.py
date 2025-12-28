8383dcf9dd4ef0796fb18169d1bfaacff695331f
Антон <mikolenko.anton1@gmail.com>
2025-12-28 23:32:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from telegram import Bot
 
 TAPO_EMAIL = os.environ["TAPO_EMAIL"]
 TAPO_PASSWORD = os.environ["TAPO_PASSWORD"]
-DEVICE_IP = os.environ["DEVICE_IP"]
-
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
@@ -17,12 +15,24 @@ async def main():
     global last_state
 
     client = ApiClient(TAPO_EMAIL, TAPO_PASSWORD)
-    device = await client.p110(DEVICE_IP)
+
+    devices = await client.get_devices()
+    p110 = None
+
+    for d in devices:
+        if d.device_type == "SMART.PLUG":
+            p110 = d
+            break
+
+    if not p110:
+        raise Exception("P110 not found in Tapo account")
+
+    device = await client.p110(p110.device_id)
     bot = Bot(BOT_TOKEN)
 
     while True:
-        info = await device.get_energy_usage()
-        power = info.current_power
+        energy = await device.get_energy_usage()
+        power = energy.current_power
         state = power > 0.5
 
         if state != last_state:
